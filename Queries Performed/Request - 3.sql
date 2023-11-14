b7de09c09903fa3be37fc71dd0bdeb977962ddf0
deveshSingh-github <144473178+deveshSingh-github@users.noreply.github.com>
2023-11-14 17:43:09 +0530

    Create Request - 3.sql

---
+++ Queries Performed/Request - 3.sql
@@ -0,0 +1,8 @@
+
+			    #####################    Request -3    #####################
+                    
+SELECT 
+      segment, count(product_code) as product_count
+      FROM dim_product
+      GROUP BY segment
+      ORDER BY product_count DESC;
